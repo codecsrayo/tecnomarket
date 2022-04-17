9cf658e49c38d7a44d886594122a9947ebc841c4
Brayan <codecsrayo@gmail.com>
2022-04-17 15:04:53 -0500

    añadido el crud de productos.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Producto
-from .forms import ContactoForm
+from .forms import ContactoForm, ProductoForm
+from django.contrib import messages
+
 
 # Create your views here.
 def home(request):
@@ -30,3 +32,51 @@ def contacto(request):
 
 def galeria(request):
     return render(request, 'app/galeria.html')
+
+def agregar_producto(request):
+    
+    data = {
+        'form': ProductoForm()
+    }
+    
+    if request.method == 'POST':
+        formulario = ProductoForm(data=request.POST, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            messages.success(request, "Producto agregado correctamente.")
+            data["form"] = formulario
+            data["mesaje"] = "No fuesposible guardar el contacto.."
+    return render(request, 'app/producto/agregar.html', data)
+
+def listar_productos(request):
+    productos = Producto.objects.all()
+    data = {
+        'productos': productos
+    }
+    
+
+    return render(request, 'app/producto/listar.html', data)
+
+
+def modificar_producto(request, id):
+    producto = get_object_or_404(Producto, id=id)
+    
+    data={
+        'form': ProductoForm(instance=producto)
+    }
+    
+    if request.method =='POST':
+        formulario = ProductoForm(data=request.POST, instance=producto, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save
+            messages.success(request, "Modificado correctamente")
+            return redirect(to="listar_productos")
+        data['form'] = formulario
+        
+    return render(request, 'app/producto/modificar.html', data)
+
+def eliminar_producto(request, id):
+    producto = get_object_or_404(Producto, id=id)
+    producto.delete()
+    messages.success(request, "Eliminado correctamente")
+    return redirect(to="listar_productos")
